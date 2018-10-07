4d025f7268dc2a36c9fb838bd5f183d595157730
smarteye <pakaudirector@gmail.com>
2018-10-07 12:49:56 +0530

    thi is the minor changes

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: "there"
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys :[keys.coockieKey]
     })
+)
 
-})
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT||5000;
 
-app.listen(PORT , ()=>{
-    console.log(" appa is listen to the port : ", PORT);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("listen to  the port : ", PORT);
 });
-
